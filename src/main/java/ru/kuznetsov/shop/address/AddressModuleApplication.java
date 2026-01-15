f2ba0f722e057fceb96f9c151adeec4e57c61f39
Pavel Kuznetsov <Technic47@gmail.com>
2026-01-15 18:09:08 +0300

    Eureka discovery connection added.

--- src/main/java/ru/kuznetsov/shop/address/AddressModuleApplication.java
+++ src/main/java/ru/kuznetsov/shop/address/AddressModuleApplication.java
@@ -2,12 +2,14 @@ package ru.kuznetsov.shop.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 import ru.kuznetsov.shop.data.config.SpringConfig;
 import ru.kuznetsov.shop.parameter.config.ParameterConfig;
 
 @SpringBootApplication
 @Import({SpringConfig.class, ParameterConfig.class})
+@EnableDiscoveryClient
 public class AddressModuleApplication {
 
     public static void main(String[] args) {
